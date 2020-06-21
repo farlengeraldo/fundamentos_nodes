bbf098ea3758c11dc739884a005d223794e52e53
farlengeraldo <farlen.geraldo@gmail.com>
2020-06-21 18:52:48 -0300

    :sparkles: desafio completado

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,17 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
+    return response.json({ transactions, balance });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -18,6 +20,20 @@ transactionRouter.get('/', (request, response) => {
 transactionRouter.post('/', (request, response) => {
   try {
     // TODO
+    // recuperar os valores do request
+    const { title, value, type } = request.body;
+    // iniciar o service
+    const createTransaction = new CreateTransactionService(
+      transactionsRepository,
+    );
+    // criar uma transacao
+    const transction = createTransaction.execute({
+      title,
+      value,
+      type,
+    });
+
+    return response.json(transction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
